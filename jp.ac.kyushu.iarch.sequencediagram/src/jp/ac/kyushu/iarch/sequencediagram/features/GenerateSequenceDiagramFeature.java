e5badb59d8648ada6514a49538f2da2d123b6d8e
Templar <templarad@gmail.com>
2014-10-09 13:45:34 +0900

    Prepare package for sequence diagram generation

--- jp.ac.kyushu.iarch.sequencediagram/src/jp/ac/kyushu/iarch/sequencediagram/features/GenerateSequenceDiagramFeature.java
+++ jp.ac.kyushu.iarch.sequencediagram/src/jp/ac/kyushu/iarch/sequencediagram/features/GenerateSequenceDiagramFeature.java
@@ -1,5 +1,16 @@
 package jp.ac.kyushu.iarch.sequencediagram.features;
 
+import java.util.List;
+
+import jp.ac.kyushu.iarch.archdsl.archDSL.Model;
+import jp.ac.kyushu.iarch.basefunction.controller.ArchModelController;
+import jp.ac.kyushu.iarch.basefunction.controller.GraphitiModelManager;
+import jp.ac.kyushu.iarch.basefunction.reader.ArchModel;
+import jp.ac.kyushu.iarch.basefunction.reader.ProjectReader;
+import jp.ac.kyushu.iarch.basefunction.reader.XMLreader;
+
+import org.eclipse.core.resources.IResource;
+import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.graphiti.features.IFeatureProvider;
 import org.eclipse.graphiti.features.context.ICustomContext;
 import org.eclipse.graphiti.features.custom.AbstractCustomFeature;
@@ -14,12 +25,22 @@ public class GenerateSequenceDiagramFeature extends AbstractCustomFeature{
 	@Override
 	public void execute(ICustomContext context) {
 		// TODO Auto-generated method stub
-		
+		XMLreader xmlreader = new XMLreader(ProjectReader.getProject());
+		List<IResource> sequenceDiagramIResources = xmlreader.getSequenceDiagramResource();
+		IResource archfile = xmlreader.getArchfileResource();
+		ArchModel archModel = new ArchModelController(archfile);
+		Model archfaceModel = archModel.getModel();
+		for(IResource sequenceDiagramIResource : sequenceDiagramIResources){
+			Resource sequenceResource = GraphitiModelManager.getGraphitiModel(sequenceDiagramIResource);
+			generateSequenceDiagram(sequenceResource,archfaceModel);
+			
+			
+		}
 	}
 
 	@Override
     public String getName() {
-        return "Generate All Class";
+        return "Generate Sequence Diagram";
     }
  
     @Override
@@ -29,6 +50,10 @@ public class GenerateSequenceDiagramFeature extends AbstractCustomFeature{
 	}
 	@Override
     public String getDescription() {
-        return "Reflecting change of code to the Class diagram.";
+        return "Reflecting change of code to the Sequence diagram.";
     }
+	
+	public void generateSequenceDiagram(Resource sequenceResource, Model archfaceModel ){
+
+	}
 }
